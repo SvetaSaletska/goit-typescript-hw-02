7c5c3fedc2b2e3b6f470957c619ade91832a79d2
SvetaSaletska <svitlanagalevich@gmail.com>
2024-02-21 00:45:37 +0200

    task05-done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactList } from '../ContactList/ContactList';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { SearchBox } from '../SearchBox/SearchBox';
@@ -7,12 +7,18 @@ import css from '../App/App.module.css';
 export const App = () => {
   const [nameFilter, setNameFilter] = useState('');
 
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContact = window.localStorage.getItem('saved-contact');
+    if (savedContact !== null) {
+      return JSON.parse(savedContact);
+    }
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  });
 
   const visibleContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(nameFilter.toLowerCase()),
@@ -29,6 +35,10 @@ export const App = () => {
       return prevContacts.filter(contact => contact.id !== userId);
     });
   };
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-contact', JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <>
